05bc8eb3a8d81fda1e05804c3b10725b48958fff
Eric Grossman <eric.josh.grossman@gmail.com>
2020-04-23 20:35:51 -0400

    readme and image

---
+++ README.md
@@ -0,0 +1,40 @@
+# gimme-state for Redux
+- super simple colored redux logger for native and web
+![](https://github.com/11grossmane/gimme-state/gimme.png)
+
+
+
+## Install
+`npm i gimme-state`
+
+## Usage
+```javascript
+import { applyMiddleware, createStore } from 'redux';
+import gimme from 'gimme-state'
+
+const store = createStore(
+  reducer,
+  applyMiddleware(gimme)
+)
+```
+
+## Options
+
+- if you use customGimme, make sure to specify before, action, and after
+- `before`: refers to the previous state
+- `action`: refers to the action you are dispatching
+- `after`: refers to the state post dispatch
+
+
+```javascript
+import { applyMiddleware, createStore } from 'redux';
+import {customGimme} from 'gimme-state'
+
+const store = createStore(
+  reducer,
+  applyMiddleware(customGimme({before:true,action:false,after:true}))
+)
+```
+
+## License
+MIT
--- src/index.js
+++ src/index.js
@@ -15,4 +15,27 @@ function logger({ getState },options={before:true,action:true,after:true}) {
     }
 }
 
+export function customGimme(options = { before: true, action: true, after: true }) {
+    return function logger({ getState }) {
+        return (next) => (action) => {
+            if (options.before) {
+                console.log('\x1b[35m%s\x1b[0m', 'BeforeDispatch: ', getState());
+            }
+            if (options.action) {
+                console.log('\x1b[36m%s\x1b[0m', 'Action: ', action);
+            }
+
+            // Call the next dispatch method in the middleware chain.
+            const returnValue = next(action);
+            if (options.after) {
+                console.log('\x1b[32m%s\x1b[0m', 'After Dispatch: ', getState());
+            }
+            // This will likely be the action itself, unless
+            // a middleware further in chain changed it.
+            return returnValue;
+        };
+    };
+}
+
+
 export default logger
