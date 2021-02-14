94762cfc3e8042dc535067df111baf71b43c19b6
Eric Grossman <eric.josh.grossman@gmail.com>
2021-02-14 13:51:38 -0600

    built

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,7 @@
 import { Dispatch, AnyAction } from 'redux';
 declare type MiddlewareReturn = (next: Dispatch<AnyAction>) => (action: any) => any;
 export declare function gimme({ getState }: any): MiddlewareReturn;
+export declare function expoGimme({ getState }: any): MiddlewareReturn;
 export interface GimmeOptions {
     [index: string]: boolean | undefined;
     before?: boolean;
